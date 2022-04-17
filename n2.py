a3fb6fbd1979f1f2d73230f761ab84a8a88ff219
Andrey1297 <84615172+Andrey1297@users.noreply.github.com>
2022-04-17 08:06:53 -0200

    Add files via upload

---
+++ n2.py
@@ -0,0 +1,15 @@
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set mode( (win_width, win_height))
+window.fill(back) 
+game = True
+finish = False 
+clock = time.Clock() 
+FPS = 60 
+racket1 = Player('racket.png', 30, 200, 4, 56, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150) 
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50) 
+font.init() font = font.Font ('calibri.ttf', 35) 
+losel = font.render(' PLAYER 1 LOSE!', True, (180, 0, 0)) 
+lose2 = font.render('PLAYER 2 LOSE!' True, 180, 0,0))
